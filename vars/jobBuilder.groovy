f1bafd83759ee4c2240d85900191dd9af6d23246
EY-UPYOG <bhartendu.jha@in.ey.com>
2026-02-18 14:36:25 +0530

    changes related to job builds

--- vars/jobBuilder.groovy
+++ vars/jobBuilder.groovy
@@ -5,7 +5,10 @@ import org.egov.jenkins.models.BuildConfig
 
 def call(Map params) {
 
-    podTemplate(yaml: """
+    String branchName = params.branch ?: "nmc-dev-main"
+    String k8sCloud = params.k8sCloud ?: "dev-cluster"
+
+    podTemplate(cloud: k8sCloud, yaml: """
 kind: Pod
 metadata:
   name: build-utils
@@ -29,7 +32,7 @@ spec:
             name: jenkins-credentials
             key: dockerPassword
       - name: DOCKER_NAMESPACE
-        value: devupyog
+        value: "devupyog"
       - name: DOCKER_GROUP_NAME  
         value: ""
     resources:
@@ -38,109 +41,83 @@ spec:
         cpu: "250m"
       limits:
         memory: "1024Mi"
-        cpu: "500m"                
-"""
-    ) {
+        cpu: "500m"
+""") {
+
         node(POD_LABEL) {
-        
-        List<String> gitUrls = params.urls;
-        String configFile = './build/build-config.yml';
-        Map<String,List<JobConfig>> jobConfigMap=new HashMap<>();
-        StringBuilder jobDslScript = new StringBuilder();
-        List<String> allJobConfigs = new ArrayList<>();
 
-        for (int i = 0; i < gitUrls.size(); i++) {
-            String dirName = Utils.getDirName(gitUrls[i]);
-            dir(dirName) {
-               //  git url: gitUrls[i], credentialsId: 'git_read'
-                   git url: gitUrls[i], credentialsId: 'git_read_https'
-                 def yaml = readYaml file: configFile;
-                 List<JobConfig> jobConfigs = ConfigParser.populateConfigs(yaml.config, env);
-                 jobConfigMap.put(gitUrls[i],jobConfigs);
-                 allJobConfigs.addAll(jobConfigs);
-            }
-        }
-        
-        Set<String> repoSet = new HashSet<>();
-        String repoList = "";
+            List<String> gitUrls = params.urls
+            String configFile = './build/build-config.yml'
 
-        List<String> folders = Utils.foldersToBeCreatedOrUpdated(allJobConfigs, env);
-                  for (int j = 0; j < folders.size(); j++) {
-                      jobDslScript.append("""
-                          folder("${folders[j]}")
-                          """);
-                    }
+            Map<String, List<JobConfig>> jobConfigMap = [:]
+            StringBuilder jobDslScript = new StringBuilder()
+            List<JobConfig> allJobConfigs = []
 
-        for (Map.Entry<Integer, String> entry : jobConfigMap.entrySet()) {   
+            // Clone repo + parse config
+            gitUrls.each { url ->
 
-            List<JobConfig> jobConfigs = entry.getValue();
+                String dirName = Utils.getDirName(url)
 
-        for (int i = 0; i < jobConfigs.size(); i++) {
+                dir(dirName) {
 
-            for(int j=0; j<jobConfigs.get(i).getBuildConfigs().size(); j++){
-                BuildConfig buildConfig = jobConfigs.get(i).getBuildConfigs().get(j);
-                repoSet.add(buildConfig.getImageName());                    
-            }  
+                    git url: url,
+                        branch: branchName,
+                        credentialsId: 'git_read_https'
 
-            repoList = String.join(",", repoSet);     
+                    def yaml = readYaml file: configFile
+                    List<JobConfig> jobConfigs = ConfigParser.populateConfigs(yaml.config, env)
 
-            jobDslScript.append("""
-            pipelineJob("${jobConfigs.get(i).getName()}") {
-                logRotator(-1, 5, -1, -1)
-                parameters {  
-                  gitParameterDefinition {
-                        name('BRANCH')
-                        type('PT_BRANCH_TAG')
-                        description('') 
-                        branch('')      
-                        useRepository('')                     
-                        defaultValue('origin/master') 
-                        branchFilter('.*')
-                        tagFilter('*')
-                        sortMode('ASCENDING_SMART')
-                        selectedValue('DEFAULT')
-                        quickFilterEnabled(true)
-                        listSize('5')                 
+                    jobConfigMap.put(url, jobConfigs)
+                    allJobConfigs.addAll(jobConfigs)
                 }
-                  booleanParam('ALT_REPO_PUSH', false, 'Check to push images to GCR')
             }
-                definition {
-                    cpsScm {
-                        scm {
-                            git{
-                                remote {
-                                    url("${entry.getKey()}")
-                                    credentials('git_read_https')
-                                } 
-                                branch ('\${BRANCH}')
-                                scriptPath('Jenkinsfile')
-                                extensions { }
+
+            // Create folder structure
+            List<String> folders = Utils.foldersToBeCreatedOrUpdated(allJobConfigs, env)
+
+            folders.each { folderName ->
+                jobDslScript.append("""
+                    folder("${folderName}")
+                """)
+            }
+
+            // Create pipeline jobs
+            jobConfigMap.each { repoUrl, jobConfigs ->
+
+                jobConfigs.each { job ->
+
+                    jobDslScript.append("""
+                        pipelineJob("dev/${job.getName()}") {
+
+                            logRotator(-1, 5, -1, -1)
+
+                            parameters {
+                                stringParam('BRANCH', '${branchName}', 'Git Branch')
+                                booleanParam('ALT_REPO_PUSH', false, 'Push to alternate repo')
+                            }
+
+                            definition {
+                                cpsScm {
+                                    scm {
+                                        git {
+                                            remote {
+                                                url("${repoUrl}")
+                                                credentials('git_read_https')
+                                            }
+                                            branch('${branchName}')
+                                            scriptPath('Jenkinsfile')
+                                        }
+                                    }
+                                }
                             }
                         }
-
-                    }
+                    """)
                 }
             }
-""");
-        }
-        }
 
-        stage('Building jobs') {
-           jobDsl scriptText: jobDslScript.toString()
+            stage('Creating Jobs') {
+                jobDsl scriptText: jobDslScript.toString()
+            }
         }
-
-/*        stage('Creating Repositories in DockerHub') {
-                    withEnv(["REPO_LIST=${repoList}"
-                    ]) {
-                        container(name: 'build-utils', shell: '/bin/sh') {
-                            sh (script:'sh /tmp/scripts/create_repo.sh')
-                           //sh (script:'echo \$REPO_LIST')
-                        }
-                    }
-        }*/
-                
-
     }
-
-}
 }
